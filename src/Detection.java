a64d4b38219a2ac95ac5f1d40c04d2c55ef5959e
xulux <xulux@DESKTOP-Q3FVEIV>
2017-12-04 11:12:22 -0800

    new

--- src/Detection.java
+++ src/Detection.java
@@ -3,9 +3,47 @@ import java.util.*;
 
 public class Detection {
 
+	@SuppressWarnings("unchecked")
 	public static void main(String[] args) throws FileNotFoundException {
-		// TODO Auto-generated method stub
-		File f = new File("DAFailTable.txt");
+		Map<Integer, Message> idMap = null;
+		idMap = process("DAFailTable.txt");
+		//store("DA", idMap);
+		idMap = (Map<Integer, Message>) read("DA");
+		Message test = idMap.get(1194);
+		System.out.println(test.toString());
+	}
+	
+	public static Object read(String fileName) {
+		try {
+			Object obj = null;
+			ObjectInputStream is = new ObjectInputStream(new FileInputStream(fileName));
+			obj = is.readObject();
+			is.close();
+			return obj;
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+		}
+		return null;
+	}
+	
+	public static void store(String fileName, Object obj) {
+		try {
+			ObjectOutputStream os = new ObjectOutputStream(new FileOutputStream(fileName));
+			os.writeObject(obj);
+			os.close();
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public static Map<Integer, Message> process(String fileName) throws FileNotFoundException {
+		File f = new File(fileName);
 		Scanner sc = new Scanner(f);
 		String line = sc.nextLine();
 		int currentID = 0;
@@ -19,6 +57,14 @@ public class Detection {
 				currentID = id;
 				idMap.put(id, new Message());
 			}
+			if (qaPair.size() < 2) {
+				if (Integer.parseInt(tokens[2]) == 0) {
+					qaPair.clear();
+					qaPair.add(tokens[1].trim());
+				} else if (Integer.parseInt(tokens[2]) == 1 && !qaPair.isEmpty()) {
+					qaPair.add(tokens[1].trim());
+				}
+			}
 			if (qaPair.size() == 2) {
 				if (!idMap.get(id).getMap().containsKey(qaPair.get(0))) {
 					idMap.get(id).getMap().put(qaPair.get(0), new LinkedList<String>());
@@ -27,28 +73,8 @@ public class Detection {
 					idMap.get(id).getMap().get(qaPair.get(0)).add(qaPair.get(1));
 				}
 				qaPair = new LinkedList<String>();
-			} else {
-				if (Integer.parseInt(tokens[2]) == 0) {
-					qaPair.add(0, tokens[1].trim());
-				} else if (Integer.parseInt(tokens[2]) == 1 && !qaPair.isEmpty()) {
-					qaPair.add(1, tokens[1].trim());
-				}
 			}
 		}
-		
-		//store("DA", idMap);
-	}
-	
-	
-	public static void store(String fileName, Object obj) {
-		try {
-			ObjectOutputStream os = new ObjectOutputStream(new FileOutputStream(fileName));
-			os.writeObject(obj);
-			os.close();
-		} catch (FileNotFoundException e) {
-			e.printStackTrace();
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
+		return idMap;
 	}
 }
--- src/Message.java
+++ src/Message.java
@@ -2,6 +2,10 @@ import java.io.Serializable;
 import java.util.*;
 
 public class Message implements Serializable{
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 874087594914412994L;
 	public Map<String, LinkedList<String>> qAndAMap;
 	
 	public Message() {
@@ -10,5 +14,13 @@ public class Message implements Serializable{
 	
 	public Map<String, LinkedList<String>> getMap() {
 		return qAndAMap;
+	}
+	
+	public String toString() {
+		String toPrint = "";
+		for (String key : qAndAMap.keySet()) {
+			toPrint += key + "\t" + qAndAMap.get(key).toString() + "\n";
+		}
+		return toPrint;
 	}
 }
