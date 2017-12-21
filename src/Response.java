59a24879ef9a2ef7b3f000c67b10b71a408dee9b
xulux <xulux@DESKTOP-Q3FVEIV>
2017-12-21 02:14:05 -0800

    Added Kmeans

--- src/Response.java
+++ src/Response.java
@@ -9,12 +9,29 @@ public class Response implements Serializable{
 	private String response;
 	private int totalLength;
 	private int numberOfTokens;
+	private int numberOfDigits = 0;
+	private int numberOfLetters = 0;
 	
 	public Response(String response) {
 		this.response = response;
 		this.totalLength = response.length();
 		String[] tokens = response.split("\\W");
 		this.numberOfTokens = tokens.length;
+		for (int i = 0; i < response.length(); i++) {
+			if (Character.isDigit(response.charAt(i))) {
+				this.numberOfDigits++;
+			} else if (Character.isLetter(response.charAt(i))) {
+				this.numberOfLetters++;
+			}
+		}
+	}
+	
+	public Response(int a, int b, int c, int d) {
+		this.response = "";
+		this.totalLength = a;
+		this.numberOfTokens = b;
+		this.numberOfDigits = c;
+		this.numberOfLetters = d;
 	}
 	
 	public int getTotalLength() {
@@ -23,6 +40,23 @@ public class Response implements Serializable{
 	
 	public int getNumberOfTokens() {
 		return this.numberOfTokens;
+	}
+	
+	public int getNumberOfDigits() {
+		return this.numberOfDigits;
+	}
+	
+	public int getNumberOfLetters() {
+		return this.numberOfLetters;
+	}
+	
+	public boolean equals(Response r) {
+		return this.response.equals(r.response);
+	}
+	
+	public double eDistance(Response r) {
+		return Math.pow(this.totalLength-r.getTotalLength(), 2) + Math.pow(this.numberOfTokens-r.getNumberOfTokens(), 2) + 
+				Math.pow(this.numberOfDigits-r.getNumberOfDigits(), 2) + Math.pow(this.numberOfLetters-r.getNumberOfLetters(), 2);
 	}
 	
 	public String toString() {
